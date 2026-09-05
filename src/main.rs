e42bd69996a82ddbac4e446550c7e3387851a1c7
wangyinhe <wyinhe@sina.cn>
2026-09-06 01:44:31 +0800

    支持resp序列化反序列化；支持echo命令

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use toy_redis::Value;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -12,6 +13,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 thread::spawn(|| {
+                    // todo thread pool
                     if let Err(e) = handle_connection(stream) {
                         eprintln!("connection error: {}", e);
                     }
@@ -28,11 +30,47 @@ fn handle_connection(mut stream: TcpStream) -> Result<()> {
     println!("accepted new connection");
     let mut buffer = [0; 512];
     loop {
-        let read_bytes = stream.read(&mut buffer)?;
-        if read_bytes == 0 {
+        let read_cnt = stream.read(&mut buffer)?;
+        if read_cnt == 0 {
             break;
         }
-        stream.write_all(b"+PONG\r\n").unwrap();
+        let input = Value::from(&buffer[..read_cnt])?;
+        println!("input is: {:?}", input);
+        let output = execute(input)?;
+
+        let mut out = Vec::with_capacity(128);
+        output.serilize(&mut out);
+        stream.write_all(&out).unwrap();
     }
     Ok(())
+}
+
+fn execute(input: Value) -> Result<Value> {
+    match input {
+        Value::Arrays(values) => {
+            let mut iter = values.iter();
+            let Some(cmd_val) = iter.next() else {
+                return Ok(Value::SimpleErrors("missing command!".into()));
+            };
+            match cmd_val {
+                Value::BulkStrings(s) | Value::SimpleStrings(s) => {
+                    let cmd = s.to_uppercase();
+                    match cmd.as_str() {
+                        "PING" => return Ok(Value::SimpleStrings("PONG".into())),
+                        "ECHO" => {
+                            let arg = iter.next().unwrap();
+                            return Ok(arg.clone());
+                        }
+                        _ => return Ok(Value::SimpleErrors("unsported command!".into())),
+                    }
+                }
+                _ => {
+                    return Ok(Value::SimpleErrors("command format error!".into()));
+                }
+            }
+        }
+        _ => {
+            return Ok(Value::SimpleErrors("input format error!".into()));
+        }
+    }
 }
---
+++ src/resp.rs
@@ -0,0 +1,227 @@
+use anyhow::{Result, bail};
+
+#[derive(Debug, Clone)]
+pub enum Value {
+    SimpleStrings(String),
+    SimpleErrors(String),
+    Integer(i32),
+    BulkStrings(String),
+    Arrays(Vec<Value>),
+}
+
+impl Value {
+    pub fn from(input: &[u8]) -> Result<Value> {
+        let mut parser = Parser::new(input);
+        parser.parse()
+    }
+
+    pub fn serilize(&self, out: &mut Vec<u8>) {
+        match self {
+            Value::SimpleStrings(s) => {
+                out.push(b'+');
+                out.extend_from_slice(s.as_bytes());
+                out.extend_from_slice(b"\r\n");
+            }
+            Value::SimpleErrors(s) => {
+                out.push(b'-');
+                out.extend_from_slice(s.as_bytes());
+                out.extend_from_slice(b"\r\n");
+            }
+            Value::Integer(i) => {
+                out.push(b':');
+                out.extend_from_slice(i.to_string().as_bytes());
+                out.extend_from_slice(b"\r\n");
+            }
+            Value::BulkStrings(s) => {
+                let bytes = s.as_bytes();
+                out.push(b'$');
+                out.extend_from_slice(bytes.len().to_string().as_bytes());
+                out.extend_from_slice(b"\r\n");
+                out.extend_from_slice(bytes);
+                out.extend_from_slice(b"\r\n");
+            }
+            Value::Arrays(vals) => {
+                out.push(b'*');
+                out.extend_from_slice(vals.len().to_string().as_bytes());
+                out.extend_from_slice(b"\r\n");
+                for val in vals {
+                    val.serilize(out);
+                }
+                out.extend_from_slice(b"\r\n");
+            }
+        }
+    }
+}
+
+struct Parser<'a> {
+    pos: usize,
+    input: &'a [u8],
+}
+
+impl<'a> Parser<'a> {
+    fn new(input: &'a [u8]) -> Self {
+        Self { input, pos: 0 }
+    }
+
+    pub fn parse(&mut self) -> Result<Value> {
+        if self.input.is_empty() {
+            bail!("can not parse empty input")
+        }
+        self.pos = 0;
+        self.parse_value()
+    }
+
+    fn parse_value(&mut self) -> Result<Value> {
+        let vt = self.input[self.pos];
+        match vt {
+            b'+' => self.simple_string(),
+            b'-' => self.simple_errors(),
+            b':' => self.integer(),
+            b'$' => self.bulk_string(),
+            b'*' => self.arrays(),
+            v => bail!("unsport type: {}", v),
+        }
+    }
+
+    fn simple_string(&mut self) -> Result<Value> {
+        self.consume(b'+')?;
+        let s = self.parse_string()?;
+        let val = Value::SimpleStrings(s);
+        self.consume_terminator()?;
+        Ok(val)
+    }
+
+    fn simple_errors(&mut self) -> Result<Value> {
+        self.consume(b'-')?;
+        let s = self.parse_string()?;
+        let val = Value::SimpleErrors(s);
+        self.consume_terminator()?;
+        Ok(val)
+    }
+
+    fn integer(&mut self) -> Result<Value> {
+        self.consume(b':')?;
+        let val = self.parse_i32()?;
+        self.consume_terminator()?;
+        Ok(Value::Integer(val))
+    }
+
+    fn bulk_string(&mut self) -> Result<Value> {
+        self.consume(b'$')?;
+        let len = self.parse_i32()? as usize;
+        self.consume_terminator()?;
+        let val = String::from_utf8(self.input[self.pos..self.pos + len].to_vec())?;
+        self.pos += len;
+        self.consume_terminator()?;
+
+        Ok(Value::BulkStrings(val))
+    }
+
+    fn arrays(&mut self) -> Result<Value> {
+        self.consume(b'*')?;
+        let len = self.parse_i32()? as usize;
+        self.consume_terminator()?;
+        let mut vals = Vec::with_capacity(len);
+        for _ in 0..len {
+            let val = self.parse_value()?;
+            vals.push(val);
+        }
+
+        Ok(Value::Arrays(vals))
+    }
+
+    fn parse_i32(&mut self) -> Result<i32> {
+        let mut flag = 1;
+        let cur = self.peek();
+        if cur == b'+' || cur == b'-' {
+            self.pos += 1;
+        }
+        if cur == b'-' {
+            flag = -1;
+        }
+        let mut val = 0;
+        while !self.is_terminator() {
+            let cur = self.advance();
+            let num = (cur - b'0') as i32;
+            if num < 0 || num > 9 {
+                bail!("illegal integer at: {}", self.pos - 1);
+            }
+            if (i32::MAX - num) / 10 < val {
+                bail!("integer overflow at: {}", self.pos);
+            }
+            val = val * 10 + num;
+        }
+        Ok(val * flag)
+    }
+
+    fn parse_string(&mut self) -> Result<String> {
+        let start = self.pos;
+        while !self.is_terminator() {
+            self.pos += 1;
+        }
+        let s = String::from_utf8(self.input[start..self.pos].to_vec())?;
+        Ok(s)
+    }
+
+    fn is_terminator(&self) -> bool {
+        &self.input[self.pos..self.pos + 2] == b"\r\n"
+    }
+
+    fn advance(&mut self) -> u8 {
+        let cur = self.peek();
+        self.pos += 1;
+        cur
+    }
+
+    fn peek(&self) -> u8 {
+        self.input.get(self.pos).copied().unwrap_or(b'\0')
+    }
+
+    fn _advance_if_eq(&mut self, val: u8) -> bool {
+        let cur = self.peek();
+        if cur == val {
+            self.pos += 1;
+            return true;
+        }
+        return false;
+    }
+
+    fn consume_terminator(&mut self) -> Result<()> {
+        self.consume(b'\r')?;
+        self.consume(b'\n')?;
+        Ok(())
+    }
+
+    fn consume(&mut self, val: u8) -> Result<()> {
+        let cur = self.peek();
+        if cur != val {
+            bail!(
+                "expected: {}, but got: {} at position: {}",
+                val as char,
+                cur as char,
+                self.pos
+            );
+        }
+        self.pos += 1;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple() {
+        let s = b"*1\r\n$4\r\nPING\r\n";
+        let value = Value::from(s).unwrap();
+        println!("{:?}", value);
+    }
+
+    #[test]
+    fn test_arr() {
+        let s = b"*2\r\n*3\r\n:1\r\n:2\r\n:3\r\n*2\r\n+Hello\r\n-World\r\n";
+        let value = Value::from(s).unwrap();
+        println!("{:?}", value);
+    }
+}
